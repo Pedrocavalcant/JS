20043a669beed6fada48dce38212f47f0fc5d747
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-02-12 23:00:06 -0300

    ajustes

--- aula14 - ex/script.js
+++ aula14 - ex/script.js
@@ -6,15 +6,15 @@ function carregar() {
     msg.innerHTML = `Agora sao ${hora} horas.`
     if(hora >=0 && hora < 12) {
         //BOM DIA
-        img.src = 'im1.png'
+        img.src = 'imagens/im1.png'
         document.body.style.background = '#e2cd9f'
     } else if(hora >= 12 && hora < 18) {
         //BOA TARDE
-        img.src = 'im2.png'
+        img.src = 'imagens/im2.png'
         document.body.style.background = '#b9846f'
     } else {
         //BOA NOITE
-        img.src = 'im3.png'
+        img.src = 'imagens/im3.png'
         document.body.style.background = '#515154'
     }
 }
