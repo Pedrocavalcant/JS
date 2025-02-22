ff3652a9df0f2837c0e117edde594c97e53a6180
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-02-22 16:08:58 -0300

    laco de repeticao

---
+++ aula16/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+
+var t = 2
+while (t <= 20) {
+    console.log(`A tabuada de 2 e ${t}`)
+    t++
+}
---
+++ aula17/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai comecar...')
+for (var c =10; c >= 1; c--){
+    console.log(`${c}`)
+}
+console.log('GO')
